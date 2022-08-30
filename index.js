166f5fbe91fe662704ea858abc0810e2f8d4eacb
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2022-08-30 05:46:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode> // using strict mode renders components twice for finding bugs
-    <App/>    // only in devolpmental process 
-  //</React.StrictMode>
+  <React.StrictMode> 
+    <App/>    
+  </React.StrictMode>
 );
 
-/*const roottwo = ReactDOM.createRoot(document.getElementById('roottwo'));
-roottwo.render(
-  <React.StrictMode>
-    <Searching />
-  </React.StrictMode>
-);*/
 
 
 
